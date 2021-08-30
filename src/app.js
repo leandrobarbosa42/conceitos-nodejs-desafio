73e6dcc05da87805b8933c5a8d56b81ce612a4fd
Leandro Barbosa <x7leandro@hotmail.com.br>
2021-08-30 19:14:30 -0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+
+const {v4: uuidv4} = require('uuid');
+
+const {validate} = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories)
+});
+
+app.post("/repositories", (request, response) => {
+  const {title, url, techs} = request.body;
+  const repositorie = {id: uuidv4(), title, url, techs};
+
+  repositories.push(repositorie);
+  
+  return response.json(repositorie)
+
+});
+
+app.put("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  // TODO
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  // TODO
+});
+
+module.exports = app;
