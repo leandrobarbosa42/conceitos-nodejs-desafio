34a4d3cbf8f445eb55d3ca187870b6f5516de12e
Leandro Barbosa <x7leandro@hotmail.com.br>
2021-08-30 21:09:05 -0300

    modify app.js

--- src/app.js
+++ src/app.js
@@ -13,12 +13,13 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
+  
   return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
   const {title, url, techs} = request.body;
-  const repositorie = {id: uuidv4(), title, url, techs};
+  const repositorie = {id: uuidv4(), title, url, techs, likes:0};
 
   repositories.push(repositorie);
   
@@ -27,15 +28,45 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const { title, techs, url} = request.body;
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+  
+  if(repositorieIndex < 0){
+    return response.status(400).json({error:'Project not found!!'})
+  };
+
+  const repositorie = {id, title, techs, url};
+
+  repositories[repositorieIndex] = repositorie;
+  
+
+  return response.json(repositorie)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+  if(repositorieIndex < 0){
+    return response.status(400).json({error:'Repositorie not found!!'})
+  };
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+  
+  if(repositorieIndex < 0){
+   return response.status(400).json({error:'Repositorie not found!!'})
+  } 
+  
+  repositories[repositorieIndex].likes++;
+  
+  return response.json(repositories[repositorieIndex])
+  
 });
 
 module.exports = app;
